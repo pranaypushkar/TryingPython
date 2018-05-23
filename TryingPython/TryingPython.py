e8ff2c24280ca07c92745ee0e0b39132873019f8
pranaypushkar92@gmail.com <pranaypushkar92@gmail.com>
2018-05-23 21:42:24 +0530

    Added conditionals

--- TryingPython/TryingPython.py
+++ TryingPython/TryingPython.py
@@ -1,2 +1,9 @@
 #Python3.6.3
 print("Hello World!")
+
+#Conditionals
+a, b = 0, 1
+if a < b:
+    print('a ({}) is less than b ({})'.format(a, b))
+else:
+    print('a ({}) is not less than b ({})'.format(a, b))
