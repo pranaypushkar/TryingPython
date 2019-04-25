86c5ba4fa31081a1ada49813856cf38db24ccfee
PRANAY PUSHKAR <pranaypushkar92@gmail.com>
2019-04-25 15:28:19 +0530

    Parsing XML in Python

---
+++ Day 4/module.py
@@ -0,0 +1,63 @@
+#movieHandler
+#https://jsfiddle.net/mkqLo9c2 python code
+#https://jsfiddle.net/058v4sw1 movies.xml
+import xml.sax
+
+class MovieHandler( xml.sax.ContentHandler ):
+   def __init__(self):
+      self.CurrentData = ""
+      self.atype = ""
+      self.format = ""
+      self.year = ""
+      self.rating = ""
+      self.stars = ""
+      self.description = ""
+
+   # Call when an element starts
+   def startElement(self, tag, attributes):
+      self.CurrentData = tag
+      if tag == "movie":
+         print ("*****Movie*****")
+         title = attributes["title"]
+         print ("Title:", title)
+
+   # Call when an elements ends
+   def endElement(self, tag):
+      if self.CurrentData == "type":
+         print ("Type:", self.type)
+      elif self.CurrentData == "format":
+         print ("Format:", self.format)
+      elif self.CurrentData == "year":
+         print ("Year:", self.year)
+      elif self.CurrentData == "rating":
+         print ("Rating:", self.rating)
+      elif self.CurrentData == "stars":
+         print ("Stars:", self.stars)
+      elif self.CurrentData == "description":
+         print ("Description:", self.description)
+      self.CurrentData = ""
+
+   # Call when a character is read
+   def characters(self, content):
+      if self.CurrentData == "type":
+         self.type = content
+      elif self.CurrentData == "format":
+         self.format = content
+      elif self.CurrentData == "year":
+         self.year = content
+      elif self.CurrentData == "rating":
+         self.rating = content
+      elif self.CurrentData == "stars":
+         self.stars = content
+      elif self.CurrentData == "description":
+         self.description = content
+
+if 1:
+   parser = xml.sax.make_parser()
+   parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+   # override the default ContextHandler
+   Handler = MovieHandler()
+   parser.setContentHandler( Handler )
+   
+   parser.parse("movies.xml")
